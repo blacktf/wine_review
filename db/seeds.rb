f03bbc3c33d85ec2c6ac278da6e35d3a202707bd
Tim Black <blacktf@gmail.com>
2015-04-03 17:59:19 -0700

    Class 02 Homework

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Wine.create(name:'Charles Shaw Cabernet Sauvignon California', year:2015, winery:'Charles Shaw', country:'USA', varietal:'Cabernet Sauvignon')
+Wine.create(name:'Charles Shaw Shiraz California', year:2015, winery:'Charles Shaw', country:'USA', varietal:'Syrah')
+Wine.create(name:'Charles Shaw Sauvignon Blanc California', year:2015, winery:'Charles Shaw', country:'USA', varietal:'Sauvignon Blanc')
+Wine.create(name:'Charles Shaw Chardonnay California', year:2015, winery:'Charles Shaw', country:'USA', varietal:'Chardonnay')
+Wine.create(name:'Cavicchioli Lambrusco Dolce 1928', year:1928, winery:'Cavicchioli', country:'Italy', varietal:'Lambrusco')
+Wine.create(name:'Jam Cellars Chardonnay Butter', year:2014, winery:'Jam Cellars', country:'USA', varietal:'Chardonnay')
