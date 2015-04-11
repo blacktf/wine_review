4900f4526ac72f178342d1cfcac141959fb9d0f3
Tim Black <blacktf@gmail.com>
2015-04-10 17:27:48 -0700

    Class 3 changes

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,9 +1,48 @@
 class WinesController < ApplicationController
+
+  before_action :set_wine,
+    only: [ :show, :edit, :update, :destroy ]
+
   def index
     @wines = Wine.all
   end
 
   def show
+  end
+
+  def new
+    @wine = Wine.new
+  end
+
+  def create
+    @wine = Wine.new(wine_params)
+    @wine.save
+    redirect_to @wine
+  end
+
+  def edit
+
+  end
+
+  def update
+    @wine.update(wine_params)
+    redirect_to @wine
+  end
+
+  def destroy
+    @wine.destroy
+    redirect_to wines_path
+  end
+
+  private
+
+  def set_wine
     @wine = Wine.find(params[:id])
   end
+
+  def wine_params
+    params.require(:wine).permit(:name, :year,
+     :winery, :country, :varietal)
+  end
+
 end
